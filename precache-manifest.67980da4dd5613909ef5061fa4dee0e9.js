aa235059dc3a8e3c922f9c78a04b024351cea1f1
Zoia <zizi2405@gmail.com>
2020-11-13 12:37:57 -0500

    Updates

---
+++ precache-manifest.67980da4dd5613909ef5061fa4dee0e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b65b97d428b169fea801de4775bade29",
+    "url": "/mobxTodo/index.html"
+  },
+  {
+    "revision": "62bed58c0bb0b85758ab",
+    "url": "/mobxTodo/static/css/main.ea632063.chunk.css"
+  },
+  {
+    "revision": "633ba01663d426468bcf",
+    "url": "/mobxTodo/static/js/2.fc80247a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/mobxTodo/static/js/2.fc80247a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62bed58c0bb0b85758ab",
+    "url": "/mobxTodo/static/js/main.a4195d1c.chunk.js"
+  },
+  {
+    "revision": "1cfc528da19d084ca72c",
+    "url": "/mobxTodo/static/js/runtime-main.304ca9f8.js"
+  }
+]);
